cce36e6c50aa4ba56c44619dc32c5f09af6e5d1f
robinrrr10 <robinrrr10@gmail.com>
2022-03-11 02:31:50 +0530

    added react native app with detox text

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import {Button, TextInput, View, SafeAreaView, Alert, Text} from 'react-native';
+const App = () => {
+	const [input, setInput] = useState('');
+	return (
+		<SafeAreaView style={{flex: 1}}>
+		 <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text testID="title" style={{fontSize: 26, textAlign: 'center'}}>Hello there</Text>
+        <TextInput
+	        testID="input"
+          placeholder="Name"
+          value={input}
+          onChangeText={setInput}
+          style={{padding: 5}}
+          />
+        <Button testID="button" title="Submit" onPress={() => Alert.alert(input)} />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ e2e/firstTest.spec.js
@@ -0,0 +1,26 @@
+describe('Example', () => {
+  before(async () => {
+    await device.launchApp();
+  });
+
+  it('should have hello text', async () => {
+    await expect(element(by.id('title'))).toBeVisible();
+  });
+
+  const typedText = 'Hello Bitch';
+
+  it('should type hello bitch', async () => {
+    const input = element(by.id('input'));
+    await input.typeText(typedText);
+  });
+
+ it('should press on the submit button', async () => {
+    await element(by.id('button')).tap();
+  });
+
+ it('should have a alert with typed text', async () => {
+    await expect(element(by.text(typedText)).atIndex(0)).toBeVisible();
+    await element(by.text("OK")).tap();
+  });
+
+});
